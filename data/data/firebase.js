64eef6a65ca866dcccb6ddc4b65c113d1ba36844
FrictionDuel <vegemoto.koemene.ch@gmail.com>
2025-11-28 18:35:10 +0900

    Update firebase.js

--- data/data/firebase.js
+++ data/data/firebase.js
@@ -4,7 +4,16 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebas
 import { getDatabase, ref, set, onValue, push, update, onDisconnect } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-database.js";
 
 
+const firebaseConfig = {// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
+  apiKey: "AIzaSyDt86-ZNqSM830xevK22NadKEyxNUxKV0A",
+  authDomain: "frictionduel.firebaseapp.com",
+  projectId: "frictionduel",
+  storageBucket: "frictionduel.firebasestorage.app",
+  messagingSenderId: "208056777710",
+  appId: "1:208056777710:web:0616eb2bcb55868612acff",
+  measurementId: "G-RRPZBWF827"
+};
 apiKey: "YOUR_API_KEY",
 authDomain: "YOUR_AUTHDOMAIN",
 databaseURL: "https://YOUR_DB.firebaseio.com",
