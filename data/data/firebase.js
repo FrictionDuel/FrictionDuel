bbac953cb0f45e39d0c98d489bbd9e2fcbe4a91c
FrictionDuel <vegemoto.koemene.ch@gmail.com>
2025-12-02 16:39:59 +0900

    Update firebase.js

--- data/data/firebase.js
+++ data/data/firebase.js
@@ -1,31 +1,26 @@
 // Firebase v9 modular style
-// 注意: このファイルは module を使って import します
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
-import { getDatabase, ref, set, onValue, push, update, onDisconnect } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-database.js";
+import { 
+  getDatabase, ref, set, onValue, push, update, onDisconnect 
+} from "https://www.gstatic.com/firebasejs/9.22.1/firebase-database.js";
 
-
-const firebaseConfig = {// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// ★ あなたの実プロジェクト設定 ★
 const firebaseConfig = {
   apiKey: "AIzaSyDt86-ZNqSM830xevK22NadKEyxNUxKV0A",
   authDomain: "frictionduel.firebaseapp.com",
+  databaseURL: "https://frictionduel-default-rtdb.firebaseio.com",
   projectId: "frictionduel",
   storageBucket: "frictionduel.firebasestorage.app",
   messagingSenderId: "208056777710",
   appId: "1:208056777710:web:0616eb2bcb55868612acff",
   measurementId: "G-RRPZBWF827"
 };
-apiKey: "YOUR_API_KEY",
-authDomain: "YOUR_AUTHDOMAIN",
-databaseURL: "https://YOUR_DB.firebaseio.com",
-projectId: "YOUR_PROJECTID",
-storageBucket: "YOUR_BUCKET",
-messagingSenderId: "SENDER_ID",
-appId: "APP_ID"
-};
 
-
+// Firebase 初期化
 const app = initializeApp(firebaseConfig);
+
+// Realtime Database
 const db = getDatabase(app);
 
-
+// 外部で使用する関数をエクスポート
 export { db, ref, set, onValue, push, update, onDisconnect };
