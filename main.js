f0bd95aa820912bcb027f8c4508a0a3ef0862d76
FrictionDuel <vegemoto.koemene.ch@gmail.com>
2025-12-02 16:41:46 +0900

    Update main.js

--- main.js
+++ main.js
@@ -88,15 +88,14 @@ qs(`#${nav}View`).classList.remove('hidden');
 
 
 // 初期化
-loadCards().then(()=>{
-// 初期化
 loadCards().then(() => {
-  // saved deck の読み込み
   const saved = JSON.parse(localStorage.getItem('deck') || '[]');
+
   if (saved.length > 0) {
     deck = saved
       .map(id => cards.find(c => c.id === id))
       .filter(Boolean);
   }
+
   renderDeck();
 });
